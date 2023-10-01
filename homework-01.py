2a11dabecd40b2426d21e1fd85190e8c60eacc5b
steklone <steklo_one@mail.ru>
2023-10-02 01:41:21 +1000

    completed task 02

--- homework-01.py
+++ homework-01.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if ((isinstance(lecturer, Lecturer) and course in lecturer.courses_attached
+                and course in self.courses_in_progress)):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +24,15 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        # print(name)
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,22 +43,23 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python', 'JavaScript']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
+best_lecturer = Lecturer('Иван', 'Иванов')
+best_lecturer.courses_attached += ['JavaScript']
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
+best_student.rate_lecturer(best_lecturer, 'JavaScript', 9)
+best_student.rate_lecturer(best_lecturer, 'JavaScript', 9)
+best_student.rate_lecturer(best_lecturer, 'JavaScript', 10)
+
 print(best_student.grades)
+print(best_student.courses_in_progress)
+print(best_lecturer.grades)
